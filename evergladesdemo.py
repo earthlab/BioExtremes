02175a4ef520a1d213e244fd6bbc71ba31250f68
fcseidl <fcseidl@umich.edu>
2023-10-16 12:58:02 -0600

    using summers 20-23

--- evergladesdemo.py
+++ evergladesdemo.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     api = L2AAPI()
     api.check_credentials()
 
-    # get all h5 files with L2A data from Summer 2020
-    urls = api.urls_in_date_range(
-        t_start=date(2020, 6, 20),
-        t_end=date(2020, 9, 22),
-        suffix='.h5'
-    )
+    # get all h5 files with L2A data from summers 2020-23
+    urls = api.urls_in_date_range(t_start=date(2020, 6, 20), t_end=date(2020, 9, 22), suffix='.h5')
+    urls += api.urls_in_date_range(t_start=date(2021, 6, 20), t_end=date(2021, 9, 22), suffix='.h5')
+    urls += api.urls_in_date_range(t_start=date(2022, 6, 21), t_end=date(2022, 9, 22), suffix='.h5')
+    urls += api.urls_in_date_range(t_start=date(2023, 6, 21), t_end=date(2023, 9, 23), suffix='.h5')
+
     # use all full power beams
     beamnames = ['BEAM0101', 'BEAM0110', 'BEAM1000', 'BEAM1011']
     # keep range of return heights
@@ -43,6 +43,7 @@ if __name__ == "__main__":
         urls,
         beamnames,
         keepobj,
+        keepevery=1,
         granuleselector=GranuleConstraint(bounds.spatial_predicate),    # TODO: put this predicate in geometry
         constraindf=Buffer(30, points),     # 30m resolution
         nproc=os.cpu_count(),
