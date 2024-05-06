77c3729f949afe7e7d166f9b11a7f60395860441
Erick Verleye <erve3705@colorado.edu>
2024-05-06 13:03:30 -0600

    Combining wind and rain

---
+++ bin/calculate_idft.py
@@ -0,0 +1,25 @@
+import sys
+from argparse import ArgumentParser
+
+from era5.extreme_events import Precipitation, Wind
+from datetime import datetime
+
+if __name__ == '__main__':
+    args = ArgumentParser()
+    args.add_argument('--thresh')
+    args.add_argument('--in_dir')
+    args.add_argument('--out_file')
+    args.add_argument('--type')
+    args.add_argument('--end_year', type=int)
+
+    args = args.parse_args()
+
+    if args.type == 'wind':
+        e = Wind(args.thresh)
+    elif args.type == 'rain':
+        e = Precipitation(args.thresh)
+    else:
+        sys.exit(2)
+
+    e.create_idft_tif(args.in_dir, datetime(1979, 1, 1), datetime(args.end_year, 12, 31),
+                      month_window=1, outfile=args.out_file)
--- era5/extreme_events.py
+++ era5/extreme_events.py
@@ -8,14 +8,14 @@ from osgeo import gdal
 import numpy as np
 
 
-class Precipitation:
+class Base:
     def __init__(self, threshold_tif: str):
         self._threshold_raster = gdal.Open(threshold_tif)
         self._threshold_array = self._threshold_raster.ReadAsArray()
         self._file_date_pattern = r'(\d{10})_(\d{10})'
 
     @staticmethod
-    def _calc_idf(bits: str, values: np.array, threshold: float, min_duration: int):
+    def _calc_idft(bits: str, values: np.array, threshold: float, min_duration: int):
         # find EWEs
         regex = '0('  # start below threshold
         regex += ''.join(['1' for _ in range(min_duration)])  # remain above threshold for ndays
@@ -30,20 +30,17 @@ class Precipitation:
             i.append((event - threshold).sum())
             d.append(event.shape[0])
 
-        print(i, d)
-
-        # report maximum intensity and duration
+        # report time since last event, frequency, and maximum intensity / duration
         i = 0. if not i else max(i)
         d = 0. if not d else max(d)
-
         f = len(matches) / values.shape[0]
-        print(i, d, f)
+        t = len(bits) - matches[-1].end()
 
-        return i, d, f
+        return i, d, f, t
 
     @staticmethod
     def _write_raster(x_size, y_size, geo_transform, projection, intensity_array, duration_array, frequency_array,
-                      outfile):
+                      time_array, outfile):
         driver = gdal.GetDriverByName('GTiff')
         output_dataset = driver.Create(outfile, x_size, y_size, 3, gdal.GDT_Float32)
         output_dataset.SetGeoTransform(geo_transform)
@@ -52,14 +49,16 @@ class Precipitation:
         output_dataset.GetRasterBand(1).WriteArray(intensity_array)
         output_dataset.GetRasterBand(2).WriteArray(duration_array)
         output_dataset.GetRasterBand(3).WriteArray(frequency_array)
+        output_dataset.GetRasterBand(4).WriteArray(time_array)
 
-    def create_idf_tif(self, in_dir: str, start_date: datetime, end_date: datetime, month_window: int, outfile: str):
+    def create_idft_tif(self, in_dir: str, start_date: datetime, end_date: datetime, window: int, outfile: str,
+                        transform=lambda x: x):
 
         bits_dict = collections.defaultdict(str)
         value_dict = collections.defaultdict(list)
-        for precipitation_file in os.listdir(in_dir):
+        for file in os.listdir(in_dir):
 
-            match = re.search(self._file_date_pattern, precipitation_file)
+            match = re.search(self._file_date_pattern, file)
 
             if match:
                 start_date_str = match.group(1)
@@ -73,9 +72,9 @@ class Precipitation:
             if not (start_date <= file_start_date and end_date <= file_end_date):
                 continue
 
-            precipitation_raster = gdal.Open(os.path.join(in_dir, precipitation_file))
-            precipitation_array = precipitation_raster.ReadAsArray()
-            x_size, y_size = precipitation_raster.RasterXSize, precipitation_raster.RasterYSize
+            raster = gdal.Open(os.path.join(in_dir, file))
+            raster_array = raster.ReadAsArray()
+            x_size, y_size = raster.RasterXSize, raster.RasterYSize
 
             for i in range(x_size):
                 for j in range(y_size):
@@ -85,25 +84,48 @@ class Precipitation:
                     if threshold <= 0:
                         continue
 
-                    bits_dict[(j, i)] += ''.join(['1' if v > threshold else '0' for v in precipitation_array[:, j, i]])
-                    value_dict[(j, i)].extend(precipitation_array[:, j, i])
+                    bits_dict[(j, i)] += ''.join(['1' if transform(v) > threshold else '0' for v in raster_array[:, j, i]])
+                    value_dict[(j, i)].extend([transform(v) for v in raster_array[:, j, i]])
 
-            del precipitation_raster
-            del precipitation_array
+            del raster
+            del raster_array
 
-        idf_dict = {}
+        idft_dict = {}
         for k, v in bits_dict.items():
-            idf_dict[k] = self._calc_idf(v, np.array(value_dict[k]), self._threshold_array[k], month_window)
+            idft_dict[k] = self._calc_idft(v, np.array(value_dict[k]), self._threshold_array[k], window)
 
         x_size, y_size = self._threshold_raster.RasterXSize, self._threshold_raster.RasterYSize
         intensity_array = np.zeros((y_size, x_size))
         duration_array = np.zeros((y_size, x_size))
         frequency_array = np.zeros((y_size, x_size))
-        for k, v in idf_dict.items():
+        time_array = np.zeros((y_size, x_size))
+        for k, v in idft_dict.items():
             intensity_array[k] = v[0]
             duration_array[k] = v[1]
             frequency_array[k] = v[2]
+            time_array[k] = v[3]
 
         self._write_raster(x_size, y_size, self._threshold_raster.GetGeoTransform(),
                            self._threshold_raster.GetProjection(), intensity_array, duration_array, frequency_array,
-                           outfile)
+                           time_array, outfile)
+
+
+class Precipitation(Base):
+    def __init__(self, threshold_tif: str):
+        super().__init__(threshold_tif)
+
+
+class Wind(Base):
+    def __init__(self, threshold_tif: str):
+        super().__init__(threshold_tif)
+
+    @staticmethod
+    def _convert_era5_wind_speed_to_ibtracs(era5_speed: float) -> float:
+        mps2kts = 1.94384
+        m, b = 0.826599091061478, 13.383383250900929
+
+        return ((era5_speed * mps2kts * m) + b) / mps2kts
+
+    def create_idft_tif(self, in_dir: str, start_date: datetime, end_date: datetime, window: int, outfile: str):
+        super().create_idft_tif(self, in_dir, start_date, end_date, window, outfile,
+                                transform=self._convert_era5_wind_speed_to_ibtracs)
